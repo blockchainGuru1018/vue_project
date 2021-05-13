aba9b6e4cb2d7539ffcaafbbd1d717e9c2de3ede
KangDo96 <as20190101@hotmail.com>
2021-05-13 11:38:53 +0800

    update readme and package.json

---
+++ src/router/layouts/auth.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <div :class="$style.container">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" module>
+@import '@design';
+
+.container {
+  display: flex;
+  min-width: 100%;
+  min-height: 100%;
+  margin: 0 auto;
+}
+</style>
